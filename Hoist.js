ff07e22cf0fa9520e1b3482700e813070aab2556
dathanwong <dwong5@villanova.edu>
2020-07-07 11:29:57 -0700

    Initial commit

---
+++ Hoist.js
@@ -0,0 +1,135 @@
+//1
+// console.log(hello);                                   
+// var hello = 'world';   
+//var hello;
+//console.log(hello); //logs undefined
+//hello = "world"
+
+//2
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+//var needle;
+//needle = "haystack"
+//needle = "magnet"
+//console.log(needle) //prints magnet
+
+//3
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+//var brendan
+//brendan = "super cool"
+//console.log(brendan) //prints super cool
+
+//4
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+//var food
+//food = chicken
+//console.log(food) //prints chicken
+//food = half-chicken
+//console.log(food) //prints half-chicken
+//food = gone
+
+//5
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+//var mean
+//crashes from undefined function
+
+//6
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+//var genre
+// console.log(genre); //prints undefined
+// genre = "disco";
+// rewind();
+// function rewind() {
+//     var genre;
+//     genre = "rock";
+//     console.log(genre); //prints rock
+//     genre = "r&b";
+//     console.log(genre); //prints r&b
+// }
+// console.log(genre); //prints disco
+
+//7
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+// dojo = "san jose"; 
+// console.log(dojo); //prints sanjose
+// learn();
+// function learn() {
+//     var dojo;
+//     dojo = "seattle"; 
+//     console.log(dojo); //prints seattle
+//     dojo = "burbank";
+//     console.log(dojo); //rints burbank
+// }
+// console.log(dojo); //prints san jose
+
+//8
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+// console.log(makeDojo("Chicago", 65)); //print out dictionary object
+// console.log(makeDojo("Berkeley", 0)); //Error trying to change dojo const
+// function makeDojo(name, students){
+//     const dojo = {}; 
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now"; 
+//     }
+//     return dojo;
+// }
